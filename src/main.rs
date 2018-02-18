b607d2ae447e805a494ff274d502755e0a945326
guni973 <ad2314ce71926@gmail.com>
2018-02-18 22:08:29 +1100

    wip tar wait

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,10 @@ extern crate time;
 
 use std::env;
 use std::path::Path;
-use std::process::Command;
-use std::process::exit;
+use std::process::{Command, exit};
 
 fn help() {
     println!("How to use Aurora");
-}
-
-fn unzip_targz(in_path: &str) {
-    let pkg_path = Path::new("/tmp/aurora/");
-    match env::set_current_dir(&pkg_path) {
-        Ok(p) => println!("{}", pkg_path.display()),
-        Err(e) => panic!("Could not change directory: {}", e),
-    };
-
-    let tar_cmd = match Command::new("tar")
-        .arg("zxvf")
-        .arg(in_path)
-        .spawn() {
-            Ok(p) => println!("Successful unzip tar.gz file: {:?}", p),
-            Err(e) => panic!("Failed unzip tar.gz: {}", e),
-        };
 }
 
 // TODO: cURL(外部コマンド)無しで実装する。
@@ -31,7 +14,7 @@ fn download_tarball(package_name: &str) {
     let now = time::now();
     let now_formated = time::strftime("%Y-%m-%d-%H-%M-%S", &now).unwrap();
     */
-    let curl_cmds = match Command::new("curl")
+    let mut curl_cmds = match Command::new("curl")
         .arg("--output")
         .arg(&format!("/tmp/aurora/{}.tar.gz", package_name))
         .arg("-L")
@@ -41,8 +24,25 @@ fn download_tarball(package_name: &str) {
         .spawn() {
             Ok(p) => p,
             Err(e) => panic!("faild tot exectute: {}", e),
-        }
+        };
     curl_cmds.wait();
+}
+
+fn unzip_targz(in_path: &str) {
+    let pkg_path = Path::new("/tmp/aurora/");
+    match env::set_current_dir(&pkg_path) {
+        Ok(p) => println!("{}", pkg_path.display()),
+        Err(e) => panic!("Could not change directory: {}", e),
+    };
+
+    let mut tar_cmd = match Command::new("tar")
+        .arg("zxvf")
+        .arg(in_path)
+        .spawn() {
+            Ok(p) => println!("Successful unzip tar.gz file: {:?}", p),
+            Err(e) => panic!("Failed unzip tar.gz: {}", e),
+        };
+    println!("{:?}", tar_cmd);
 }
 
 fn makepkg(path: &str) {
@@ -63,7 +63,13 @@ fn makepkg(path: &str) {
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 { help(); exit(0); }
-    for package in &args[1..] {
+
+    match args[1].as_str() {
+        "-S" => println!("Install package"),
+        _    => println!("Unexpected arguments"),
+    }
+
+    for package in &args[2..] {
         download_tarball(package);
         unzip_targz(&format!("/tmp/aurora/{}.tar.gz", package.to_string()));
         makepkg(&format!("/tmp/aurora/{}", package.to_string()));
