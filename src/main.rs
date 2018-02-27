a74e38707502c41cebce5198672e9576ff78abdb
guni973 <ad2314ce71926@gmail.com>
2018-02-27 18:02:17 +1100

    refactor and mode divide

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-extern crate time;
-
 use std::env;
 use std::path::Path;
 use std::process::Command;
@@ -9,47 +7,14 @@ fn help() {
     println!("How to use Aurora");
 }
 
-fn struct_cmd(cmd: &str, args: &[&str]) -> Command {
-    let mut cmd = Command::new(cmd);
-    for arg in args {
-        cmd.arg(arg);
-    }
-    cmd
-}
-
-fn unzip_targz(in_path: &str) {
-    let pkg_path = Path::new("/tmp/aurora/");
-    match env::set_current_dir(&pkg_path) {
-        Ok(p) => println!("{}", pkg_path.display()),
-        Err(e) => panic!("Could not change directory: {}", e),
-    };
-
-    let tar_cmd = match Command::new("tar")
-        .arg("zxvf")
-        .arg(in_path)
-        .spawn() {
-            Ok(p) => println!("Successful unzip tar.gz file: {:?}", p),
-            Err(e) => panic!("Failed unzip tar.gz: {}", e),
-        };
-}
-
 fn download_tarball(package_name: &str) {
-    let now = time::now();
-    let now_formated = time::strftime("%Y-%m-%d-%H-%M-%S", &now).unwrap();
-    let curl_args = [
-        "curl",
-        "--output",
-        &format!("/tmp/aurora/{0}_{1}.tar.gz", package_name, &now_formated),
-        "-L",
-        "-O",
-        &format!("https://aur.archlinux.org/cgit/aur.git/snapshot/{}.tar.gz",
-                 package_name),
-    ];
-    let mut curl_cmds = match struct_cmd(curl_args[0], &curl_args[1..]).spawn() {
-        Ok(p) => p,
-        Err(e) => panic!("faild tot exectute: {}", e),
-    };
-    curl_cmds.wait();
+    let aur_url = &format!("https://aur.archlinux.org/{}.git", package_name);
+    let clone_path = &format!("/tmp/aurora/{}", package_name);
+    let mut git_clone = Command::new("git")
+        .arg("clone").arg(aur_url).arg(clone_path)
+        .spawn()
+        .unwrap();
+    git_clone.wait();
 }
 
 fn makepkg(path: &str) {
@@ -66,13 +31,20 @@ fn makepkg(path: &str) {
     cmd.wait();
 }
 
+fn sync(options: &[String]) {
+    for package in options {
+        download_tarball(package);
+        // unzip_targz(&format!("/tmp/aurora/{}.tar.gz", package.to_string()));
+        makepkg(&format!("/tmp/aurora/{}", package.to_string()));
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 { help(); exit(0); }
-    for package in &args[1..] {
-        download_tarball(package);
-        unzip_targz(&format!("/tmp/aurora/{}.tar.gz", package.to_string()));
-        makepkg(&format!("/tmp/aurora/{}", package.to_string()));
+
+    match &args[1][..] {
+        "-S" => sync(&args[2..]),
+        _ => help(),
     }
 }
