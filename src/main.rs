a172d1f5123dfc1bc73f3721fd6d77ecf38293ae
guni1192 <ad2314ce71926@gmail.com>
2019-02-03 15:07:52 +0000

    could spec multiple packages

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use clap::{App, Arg};
 mod sync;
 // mod query;
 
+
 fn main() {
     let matches = App::new("aurora")
         .version("0.1.0")
@@ -14,12 +15,12 @@ fn main() {
                 .long("sync")
                 .value_name("FORMULA...")
                 .help("set repository name")
+                .multiple(true)
                 .takes_value(true),
         )
         .get_matches();
 
-    if let Some(s) = matches.value_of("sync") {
-        println!("Install {} ...", s);
-        sync::sync(s);
+    if let Some(s) = matches.values_of("sync") {
+        sync::sync(s.collect());
     }
 }
--- src/sync.rs
+++ src/sync.rs
@@ -1,10 +1,14 @@
 use std::env;
+use std::io;
 use std::path::Path;
 use std::process::Command;
 
-fn download_tarball(package_name: &str) -> std::io::Result<()> {
-    let aur_url = &format!("https://aur.archlinux.org/{}.git", package_name);
-    let clone_path = &format!("/tmp/aurora/{}", package_name);
+static AUR_BASE_URL: &'static str = "https://aur.archlinux.org/";
+static DOWNLOAD_PATH: &'static str = "/tmp/aurora";
+
+fn download_tarball(package_name: &str) -> io::Result<()> {
+    let aur_url = &format!("{}/{}.git", AUR_BASE_URL, package_name);
+    let clone_path = &format!("{}/{}", DOWNLOAD_PATH, package_name);
     let mut git_clone = Command::new("git")
         .arg("clone")
         .arg(aur_url)
@@ -15,8 +19,8 @@ fn download_tarball(package_name: &str) -> std::io::Result<()> {
     Ok(())
 }
 
-fn makepkg(package_name: &str) -> std::io::Result<()> {
-    let path = &format!("/tmp/aurora/{}", package_name.to_string());
+fn makepkg(package_name: &str) -> io::Result<()> {
+    let path = &format!("{}/{}", DOWNLOAD_PATH, package_name.to_string());
     let pkg_path = Path::new(path);
 
     env::set_current_dir(&pkg_path).expect("Could not change directory: ");
@@ -27,9 +31,12 @@ fn makepkg(package_name: &str) -> std::io::Result<()> {
     Ok(())
 }
 
-pub fn sync(package_name: &str) {
-    download_tarball(package_name).expect("Could not download aur tar: ");
-    makepkg(package_name).expect("Could not makepkg: ");
+pub fn sync(package_names: Vec<&str>) {
+    for package_name in package_names {
+        println!("Install {:?} ...", package_name);
+        download_tarball(package_name).expect("Could not download aur tar: ");
+        makepkg(package_name).expect("Could not makepkg: ");
+    }
 }
 
 // fn search(options: &[String]) {
